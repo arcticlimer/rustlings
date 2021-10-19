1746adeb0495ae19eca26f8e9813d9dbac5a1689
Vinícius Müller <vinigm.nho@gmail.com>
2021-10-18 21:51:20 -0300

    Add nix flake and start doing rustlings

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Algorithms and data structures";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          rustlings = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "rustlings";
+            version = "4.6.0";
+            src = ./.;
+            cargoSha256 = "sha256-t29XFL0xbYZLwKCs+tLg92fgB46XO+cMYkEm325iJgw=";
+          };
+        in
+          rec {
+            devShell = pkgs.mkShell {
+              buildInputs = with pkgs; [ rustc cargo rustlings ];
+            };
+          }
+    );
+}
