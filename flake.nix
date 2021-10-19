fc56bfe266a76921923084529ab5e10db0665c82
Vinícius Müller <vinigm.nho@gmail.com>
2021-10-19 11:00:18 -0300

    Add rust-analyzer and do more exercises

--- flake.nix
+++ flake.nix
@@ -6,19 +6,26 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          rustlings = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "rustlings";
-            version = "4.6.0";
-            src = ./.;
-            cargoSha256 = "sha256-t29XFL0xbYZLwKCs+tLg92fgB46XO+cMYkEm325iJgw=";
-          };
-        in
-          rec {
-            devShell = pkgs.mkShell {
-              buildInputs = with pkgs; [ rustc cargo rustlings ];
-            };
-          }
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustlings = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "rustlings";
+          version = "4.6.0";
+          src = ./.;
+          cargoSha256 = "sha256-t29XFL0xbYZLwKCs+tLg92fgB46XO+cMYkEm325iJgw=";
+          doCheck = false;
+        };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustlings
+            rust-analyzer
+            rustfmt
+          ];
+        };
+      }
     );
 }
